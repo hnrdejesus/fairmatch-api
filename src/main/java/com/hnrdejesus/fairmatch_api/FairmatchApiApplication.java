546bbe232cec9a90d8c2bffed0f17d5dd3671255
Henrique <phdejesus1998@gmail.com>
2026-05-05 16:01:28 -0300

    chore: initial project setup with PostgreSQL

---
+++ src/main/java/com/hnrdejesus/fairmatch_api/FairmatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.hnrdejesus.fairmatch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairmatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairmatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnrdejesus/fairmatch_api/FairmatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnrdejesus.fairmatch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairmatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
